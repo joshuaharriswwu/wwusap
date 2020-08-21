301072f8877b96d3b0ee365ecca4a137039855a0
josh.harris@wwutilities.co.uk <Josh.Harris@wwutilities.co.uk>
2020-08-21 14:12:21 +0100

    updated hana lvm disks

--- ARMTemplates/serverTemplates/disks/lvm/hana-lvm-3datadisks2logdisks.sh
+++ ARMTemplates/serverTemplates/disks/lvm/hana-lvm-3datadisks2logdisks.sh
@@ -1,58 +1,68 @@
-sudo zypper install lvm2
-
-# Creating the /hana/data volume
-sudo pvcreate /dev/sdc
-sudo pvcreate /dev/sdd
-sudo pvcreate /dev/sde
-
-sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde 
-sudo lvcreate --extents 100%FREE --stripes 3 --stripesize 256 --name data-lv01 data-vg01
-sudo mkfs.xfs /dev/data-vg01/data-lv01
-
-sudo mkdir /hana /hana/data
-# Update fstab
-echo "/dev/data-vg01/data-lv01  /hana/data  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
-
-# Creating the /hana/log volume
-sudo pvcreate /dev/sdf
-sudo pvcreate /dev/sdg
-sudo vgcreate log-vg01 /dev/sdf /dev/sdg
-sudo lvcreate --extents 100%FREE --stripes 2 --stripesize 32  --name log-lv01 log-vg01
-sudo mkfs.xfs /dev/log-vg01/log-lv01
-
-sudo mkdir /hana/log
-# Update fstab
-echo "/dev/log-vg01/log-lv01  /hana/log  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
-
-# Creating the /hana/shared volume
-sudo pvcreate /dev/sdh
-sudo vgcreate shared-vg01 /dev/sdh
-sudo lvcreate --extents 100%FREE --name shared-lv01 shared-vg01
-sudo mkfs.xfs /dev/shared-vg01/shared-lv01
-
-sudo mkdir /hana/shared
-# Update fstab
-echo "/dev/shared-vg01/shared-lv01 /hana/shared  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
-
-# Creating the /usr/sap volume
-sudo pvcreate /dev/sdi
-sudo vgcreate usrsap-vg01 /dev/sdi
-sudo lvcreate --extents 100%FREE --name usrsap-lv01 usrsap-vg01
-sudo mkfs.xfs /dev/usrsap-vg01/usrsap-lv01
-
-sudo mkdir /usr/sap
-# Update fstab
-echo "/dev/usrsap-vg01/usrsap-lv01 /usr/sap  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
-
-# Creating the /hana/backup volume
-sudo pvcreate /dev/sdj
-sudo vgcreate backup-vg01 /dev/sdj
-sudo lvcreate --extents 100%FREE --stripes 3 --name backup-lv01 backup-vg01
-sudo mkfs.xfs /dev/backup-vg01/backup-lv01
-
-sudo mkdir /hana/backup
-
-echo "/dev/backup-vg01/backup-lv01  /hana/backup  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
-
+#!/bin/bash
+ 
+## This script assumes the following data disk LUN layout
+#LUN TYPE  MOUNTPOINT
+# 0 - P6 (/hana/data #1)
+# 1 - P6 (/hana/data #2)
+# 2 - P6 (/hana/data #3)
+# 3 - P6 (/hana/data #4)
+# 4 - P10 (/hana/log #1)
+# 5 - P10 (/hana/log #2)
+# 6 - P10 (/hana/log #3)
+# 7 - P20 (/hana/shared)
+# 8 - P6 (/usr/sap)
+ 
+# Ensure LVM is installed and up to date
+sudo zypper install -y lvm2
+ 
+# Pre-stage the mount directories
+sudo mkdir -p /hana/data
+sudo mkdir -p /hana/log
+sudo mkdir -p /hana/shared
+sudo mkdir -p /usr/sap
 sudo chmod -R 0755 /hana
 sudo chmod -R 0755 /usr/sap
+ 
+# Creating the /hana/data volume
+sudo pvcreate /dev/disk/azure/scsi1/lun[0123]
+sudo vgcreate data-vg01 /dev/disk/azure/scsi1/lun[0123]
+sudo lvcreate --extents 100%FREE --stripes 4 --stripesize 256 --name data-lv01 data-vg01
+sudo mkfs.xfs /dev/data-vg01/data-lv01
+ 
+# Update fstab
+fs_uuid=$(blkid -o value -s UUID /dev/data-vg01/data-lv01)
+echo "/dev/disk/by-uuid/$fs_uuid  /hana/data  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+ 
+# Creating the /hana/log volume
+sudo pvcreate /dev/disk/azure/scsi1/lun[456]
+sudo vgcreate log-vg01 /dev/disk/azure/scsi1/lun[456]
+sudo lvcreate --extents 100%FREE --stripes 2 --stripesize 64 --name log-lv01 log-vg01
+sudo mkfs.xfs /dev/log-vg01/log-lv01
+ 
+# Update fstab
+fs_uuid=$(blkid -o value -s UUID /dev/log-vg01/log-lv01)
+echo "/dev/disk/by-uuid/$fs_uuid  /hana/log  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+ 
+# Creating the /hana/shared volume
+sudo pvcreate /dev/disk/azure/scsi1/lun7
+sudo vgcreate shared-vg01 /dev/disk/azure/scsi1/lun7
+sudo lvcreate --extents 100%FREE --name shared-lv01 shared-vg01
+sudo mkfs.xfs /dev/shared-vg01/shared-lv01
+ 
+# Update fstab
+fs_uuid=$(blkid -o value -s UUID /dev/shared-vg01/shared-lv01)
+echo "/dev/disk/by-uuid/$fs_uuid /hana/shared  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+ 
+ 
+# Creating the /usr/sap volume
+sudo pvcreate /dev/disk/azure/scsi1/lun8
+sudo vgcreate usrsap-vg01 /dev/disk/azure/scsi1/lun8
+sudo lvcreate --extents 100%FREE --name usrsap-lv01 usrsap-vg01
+sudo mkfs.xfs /dev/usrsap-vg01/usrsap-lv01
+ 
+# Update fstab
+fs_uuid=$(blkid -o value -s UUID /dev/usrsap-vg01/usrsap-lv01)
+echo "/dev/disk/by-uuid/$fs_uuid /usr/sap  xfs  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+ 
+# Mount the new filesystems
+sudo mount -aupdated
